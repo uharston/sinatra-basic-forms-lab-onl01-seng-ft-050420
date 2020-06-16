ec7704045229afe6105f2355fede7f5e9afaafbb
uharston <uharston@gmail.com>
2020-06-16 13:53:52 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do 
+        erb :index 
+    end 
+
+    get '/new' do 
+    
+        erb :create_puppy 
+    end 
+
+    post '/puppy' do 
+        puts params 
+        @puppy = Puppy.new(params)
+        erb :display_puppy
+    end 
+
 end
--- models/puppy.rb
+++ models/puppy.rb
@@ -1,1 +1,14 @@
 # since we are not using ActiveRecord in this lab, you write a bare Ruby model (like you did in MOD1)
+class Puppy 
+    attr_accessor :name, :breed, :age 
+
+    def initialize(params)
+        @name = params[:name]
+        @breed = params[:breed]
+        @age = params[:age]
+        # hash.each do |key, value|
+        #     send("#{key}=", value)
+        # end 
+    end 
+
+end 
